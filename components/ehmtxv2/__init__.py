e609141b3324f32a1474929b104e7b54c8988ac6
LuBeDa <lb@sell-e.de>
2023-06-03 15:23:29 +0200

    message

--- components/ehmtxv2/__init__.py
+++ components/ehmtxv2/__init__.py
@@ -28,7 +28,7 @@ SVG_FULL_SCREEN_START = '<svg width="320px" height="80px" viewBox="0 0 320 80">'
 SVG_END = "</svg>"
 
 logging.warning(f"")
-logging.warning(f"This is a beta version of https://github.com/lubeda/EspHoMaTriXv2")
+logging.warning(f"Please check the documentation and wiki https://github.com/lubeda/EspHoMaTriXv2")
 logging.warning(f"")
 
 def rgb565_svg(x,y,r,g,b):
--- wiki/home.md
+++ wiki/home.md
@@ -1,6 +1,6 @@
 # Welcome to the EspHoMaTriXv2 wiki
 
-## usage examples
+## Usage examples
 
 - [Display a timer on your ehmtxv2](display-timer)
 - [weather display](weather-display)
