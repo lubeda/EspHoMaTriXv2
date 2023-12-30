f720191b6bbee4789d2f943dca2d28b6d2f3eb9b
LuBeDa <lb@sell-e.de>
2023-12-30 14:08:34 +0100

    cleaned up

--- components/ehmtxv2/EHMTX.h
+++ components/ehmtxv2/EHMTX.h
@@ -28,7 +28,7 @@ const uint8_t TEXTSCROLLSTART = 8;
 const uint8_t TEXTSTARTOFFSET = (32 - 8);
 
 const uint16_t POLLINGINTERVAL = 250;
-static const char *const EHMTX_VERSION = "2023.9.1";
+static const char *const EHMTX_VERSION = "2024.1.1";
 static const char *const TAG = "EHMTXv2";
 
 enum show_mode : uint8_t
