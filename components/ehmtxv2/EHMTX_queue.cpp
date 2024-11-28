33d49aec2dd030fae24ea3eccc40aee4f3437dc7
lubeda <lb@sell-e.de>
2024-11-28 12:42:30 +0100

    add clipping

--- components/ehmtxv2/EHMTX_queue.cpp
+++ components/ehmtxv2/EHMTX_queue.cpp
@@ -885,6 +885,7 @@ namespace esphome
       case MODE_RAINBOW_ICON:
       case MODE_ICON_PROGRESS:
       case MODE_PROGNOSIS_SCREEN:
+        this->config_->display->start_clipping(8,0,31,7);
         color_ = (this->mode == MODE_RAINBOW_ICON || this->mode == MODE_RAINBOW_ALERT_SCREEN) ? this->config_->rainbow_color : this->text_color;
 #ifdef EHMTXv2_USE_RTL
         this->config_->display->print(this->xpos() + xoffset, this->ypos() + yoffset, font, color_, esphome::display::TextAlign::BASELINE_RIGHT,
@@ -898,6 +899,8 @@ namespace esphome
         this->config_->display->print(this->xpos() + xoffset, this->ypos() + yoffset, font, color_, esphome::display::TextAlign::BASELINE_LEFT,
                                       this->text.c_str());
 #endif
+        this->config_->display->start_clipping(0,0,0,0);
+        
         if (this->mode == MODE_ICON_PROGRESS)
         {
           if (this->icon != BLANKICON)
@@ -1003,6 +1006,7 @@ namespace esphome
             }
           }
         }
+        
         break;
 
       case MODE_TEXT_PROGRESS:
@@ -1033,6 +1037,7 @@ namespace esphome
 
       case MODE_ICON_TEXT_SCREEN:
       case MODE_RAINBOW_ICON_TEXT_SCREEN:
+        this->config_->display->start_clipping(8,0,31,7);
         color_ = (this->mode == MODE_RAINBOW_ICON_TEXT_SCREEN) ? this->config_->rainbow_color : this->text_color;
 #ifdef EHMTXv2_USE_RTL
         this->config_->display->print(this->xpos() + xoffset, this->ypos() + yoffset, font, color_, esphome::display::TextAlign::BASELINE_RIGHT,
@@ -1046,6 +1051,7 @@ namespace esphome
         this->config_->display->print(this->xpos() + xoffset, this->ypos() + yoffset, font, color_, esphome::display::TextAlign::BASELINE_LEFT,
                                       this->text.c_str());
 #endif
+        this->config_->display->start_clipping(0,0,0,0);
         if (this->icon != BLANKICON)
         {
           int x = 0;
