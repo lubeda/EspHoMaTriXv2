3e1a05478a87896d9715b4debdf9c28b14457806
lubeda <lb@sell-e.de>
2024-12-02 16:38:49 +0100

    version

--- components/ehmtxv2/EHMTX.h
+++ components/ehmtxv2/EHMTX.h
@@ -38,7 +38,7 @@ const uint8_t TEXTSCROLLSTART = 8;
 const uint8_t TEXTSTARTOFFSET = (32 - 8);
 
 const uint16_t POLLINGINTERVAL = 250;
-static const char *const EHMTX_VERSION = "2024.6.0";
+static const char *const EHMTX_VERSION = "2024.11.0";
 static const char *const TAG = "EHMTXv2";
 
 enum show_mode : uint8_t
--- components/ehmtxv2/__init__.py
+++ components/ehmtxv2/__init__.py
@@ -32,7 +32,6 @@ SVG_END = "</svg>"
 
 logging.warning(f"")
 logging.warning(f"Please check the documentation and wiki https://github.com/lubeda/EspHoMaTriXv2")
-logging.warning(f"This will only work with esphome >= 2023.7.0")
 logging.warning(f"")
 
 def rgb565_svg(x,y,r,g,b):
