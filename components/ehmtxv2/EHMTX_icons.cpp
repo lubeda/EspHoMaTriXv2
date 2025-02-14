f0528ea5e3168ed6ed629801a9513d8b98cdecd9
Sergio Conde <sconde@tinybird.co>
2025-02-14 10:48:03 +0100

    Required Animation changes for ESPHome 2025.2.0

--- components/ehmtxv2/EHMTX_icons.cpp
+++ components/ehmtxv2/EHMTX_icons.cpp
@@ -3,14 +3,13 @@
 namespace esphome
 {
 
-  EHMTX_Icon::EHMTX_Icon(const uint8_t *data_start, int width, int height, uint32_t animation_frame_count, esphome::image::ImageType type, std::string icon_name, bool revers, uint16_t frame_duration)
-      : Animation(data_start, width, height, animation_frame_count, type)
+  EHMTX_Icon::EHMTX_Icon(const uint8_t *data_start, int width, int height, uint32_t animation_frame_count, esphome::image::ImageType type, std::string icon_name, bool revers, uint16_t frame_duration, esphome::image::Transparency transparency)
+      : Animation(data_start, width, height, animation_frame_count, type, transparency)
   {
     this->name = icon_name;
     this->reverse = revers;
     this->frame_duration = frame_duration;
     this->counting_up = true;
-    this->transparent_=false;
   }
 
   void EHMTX_Icon::next_frame()
