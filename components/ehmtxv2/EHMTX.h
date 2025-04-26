61da3246764389dad166bfb34215b333b430e374
Dieter Tschanz <dieter.tschanz@geistt.com>
2025-04-26 09:13:11 +0200

    Added string formatting macro to build on esp-idf.

--- components/ehmtxv2/EHMTX.h
+++ components/ehmtxv2/EHMTX.h
@@ -4,13 +4,14 @@
 #define USE_Fireplugin
 #include "esphome/components/time/real_time_clock.h"
 
+#ifndef F
+  #define F(x) (x)
+#endif
+
 #if defined CONFIG_IDF_TARGET_ESP32S3 || defined CONFIG_IDF_TARGET_ESP32
   #undef EHMTXv2_RAINBOW_SHIMMER
   #pragma warning ( "with IDF-Framework no Fire and Rainbowshimmer") 
   #undef USE_Fireplugin
-  #ifndef F
-  //  #define F(x) (x)
-  #endif
 #endif
 
 const uint8_t MAXQUEUE = 24;
