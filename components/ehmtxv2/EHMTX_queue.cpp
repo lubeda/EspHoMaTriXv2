ae2c1b2059f2901831603148907afcd948e0f1cb
Andrew J.Swan <ajs.blackhole@gmail.com>
2025-06-13 15:28:32 +0300

    2025.5.3: Remove `Screen Clipping`

--- components/ehmtxv2/EHMTX_queue.cpp
+++ components/ehmtxv2/EHMTX_queue.cpp
@@ -887,7 +887,6 @@ namespace esphome
       case MODE_RAINBOW_ICON:
       case MODE_ICON_PROGRESS:
       case MODE_PROGNOSIS_SCREEN:
-        this->config_->display->start_clipping(8,0,31,7);
         color_ = (this->mode == MODE_RAINBOW_ICON || this->mode == MODE_RAINBOW_ALERT_SCREEN) ? this->config_->rainbow_color : this->text_color;
 #ifdef EHMTXv2_USE_RTL
         this->config_->display->print(this->xpos() + xoffset, this->ypos() + yoffset, font, color_, esphome::display::TextAlign::BASELINE_RIGHT,
@@ -900,7 +899,6 @@ namespace esphome
 #endif
           this->config_->draw_text(this->text, font, color_, this->xpos() + xoffset, this->ypos() + yoffset);
 #endif
-        this->config_->display->start_clipping(0,0,0,0);
         
         if (this->mode == MODE_ICON_PROGRESS)
         {
@@ -1038,7 +1036,6 @@ namespace esphome
 
       case MODE_ICON_TEXT_SCREEN:
       case MODE_RAINBOW_ICON_TEXT_SCREEN:
-        this->config_->display->start_clipping(8,0,31,7);
         color_ = (this->mode == MODE_RAINBOW_ICON_TEXT_SCREEN) ? this->config_->rainbow_color : this->text_color;
 #ifdef EHMTXv2_USE_RTL
         this->config_->display->print(this->xpos() + xoffset, this->ypos() + yoffset, font, color_, esphome::display::TextAlign::BASELINE_RIGHT,
@@ -1051,7 +1048,6 @@ namespace esphome
 #endif
           this->config_->draw_text(this->text, font, color_, this->xpos() + xoffset, this->ypos() + yoffset);
 #endif
-        this->config_->display->start_clipping(0,0,0,0);
         if (this->icon != BLANKICON)
         {
           int32_t x = 0;
