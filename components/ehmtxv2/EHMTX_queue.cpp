9c251dbbfddcbcbbd89d046e41e950a459c4b16f
Andrew J.Swan <ajs.blackhole@gmail.com>
2025-06-13 15:55:11 +0300

    2025.5.3: Extend `Screen Clipping`

--- components/ehmtxv2/EHMTX_queue.cpp
+++ components/ehmtxv2/EHMTX_queue.cpp
@@ -887,6 +887,7 @@ namespace esphome
       case MODE_RAINBOW_ICON:
       case MODE_ICON_PROGRESS:
       case MODE_PROGNOSIS_SCREEN:
+        this->config_->display->start_clipping(8,0,32,8);
         color_ = (this->mode == MODE_RAINBOW_ICON || this->mode == MODE_RAINBOW_ALERT_SCREEN) ? this->config_->rainbow_color : this->text_color;
 #ifdef EHMTXv2_USE_RTL
         this->config_->display->print(this->xpos() + xoffset, this->ypos() + yoffset, font, color_, esphome::display::TextAlign::BASELINE_RIGHT,
@@ -899,7 +900,8 @@ namespace esphome
 #endif
           this->config_->draw_text(this->text, font, color_, this->xpos() + xoffset, this->ypos() + yoffset);
 #endif
-        
+        this->config_->display->end_clipping();
+
         if (this->mode == MODE_ICON_PROGRESS)
         {
           if (this->icon != BLANKICON)
@@ -1036,6 +1038,7 @@ namespace esphome
 
       case MODE_ICON_TEXT_SCREEN:
       case MODE_RAINBOW_ICON_TEXT_SCREEN:
+        this->config_->display->start_clipping(8,0,32,8);
         color_ = (this->mode == MODE_RAINBOW_ICON_TEXT_SCREEN) ? this->config_->rainbow_color : this->text_color;
 #ifdef EHMTXv2_USE_RTL
         this->config_->display->print(this->xpos() + xoffset, this->ypos() + yoffset, font, color_, esphome::display::TextAlign::BASELINE_RIGHT,
@@ -1048,6 +1051,8 @@ namespace esphome
 #endif
           this->config_->draw_text(this->text, font, color_, this->xpos() + xoffset, this->ypos() + yoffset);
 #endif
+        this->config_->display->end_clipping();
+
         if (this->icon != BLANKICON)
         {
           int32_t x = 0;
