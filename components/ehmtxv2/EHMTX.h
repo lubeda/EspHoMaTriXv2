b13057f5290c8cc3f52fa712a3e77f51cca8b632
Andrew J.Swan <ajs.blackhole@gmail.com>
2025-06-23 10:08:31 +0300

    2025.6.1: Version Bump

--- components/ehmtxv2/EHMTX.h
+++ components/ehmtxv2/EHMTX.h
@@ -36,7 +36,7 @@ const uint8_t TEXTSCROLLSTART = 8;
 const uint8_t TEXTSTARTOFFSET = (32 - 8);
 
 const uint16_t POLLINGINTERVAL = 250;
-static const char *const EHMTX_VERSION = "2025.6.0";
+static const char *const EHMTX_VERSION = "2025.6.1";
 static const char *const TAG = "EHMTXv2";
 
 enum show_mode : uint8_t
